a1deb5660640b7fa368fbac478a34aba24c37256
pfijen <paulo.fijen@gmail.com>
2021-02-13 16:19:04 +0100

    added docker and postgres

--- project/app/main.py
+++ project/app/main.py
@@ -1,12 +1,22 @@
 # project/app/main.py
 
+import os
 
 from fastapi import Depends, FastAPI
+from tortoise.contrib.fastapi import register_tortoise
 
 from app.config import Settings, get_settings
 
 app = FastAPI()
 
+
+register_tortoise(
+    app,
+    db_url=os.environ.get("DATABASE_URL"),
+    modules={"models": ["app.models.tortoise"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
 
 @app.get("/ping")
 async def pong(settings: Settings = Depends(get_settings)):
