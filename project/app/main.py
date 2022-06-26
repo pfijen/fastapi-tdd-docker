371797a640b2ca79fecec92a10b3f11a42071790
Paulo Fijen <paulofijen@Paulos-MacBook-Pro.local>
2022-06-26 22:51:58 +0200

    project setup

--- project/app/main.py
+++ project/app/main.py
@@ -1,16 +1,29 @@
 # project/app/main.py
 
+import os
 
 from fastapi import FastAPI, Depends
+from tortoise.contrib.fastapi import register_tortoise
 
 from app.config import get_settings, Settings
+
+
 app = FastAPI()
+
+
+register_tortoise(
+    app,
+    db_url=os.environ.get("DATABASE_URL"),
+    modules={"models": ["app.models.tortoise"]},
+    generate_schemas=False,
+    add_exception_handlers=True,
+)
 
 
 @app.get("/ping")
 async def pong(settings: Settings = Depends(get_settings)):
     return {
-        "ping": "pong!", 
-        "environment": settings.environment, 
+        "ping": "pong!",
+        "environment": settings.environment,
         "testing": settings.testing
     }
